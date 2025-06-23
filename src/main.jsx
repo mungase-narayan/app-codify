8e0a2d110dbb2b27b99499a787f1b3cc843cf703
mungase-narayan <narayanmungase03@gmail.com>
2025-06-23 21:46:46 +0530

    add: Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import { createRoot } from "react-dom/client";
+import { Toaster } from "@/components/ui/sonner";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+import AppRoutes from "./routes";
+
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <AppRoutes/>
+          <Toaster />
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
