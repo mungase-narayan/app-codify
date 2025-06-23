8f28f8964d39e9b7c0535de0eca954d93a77cb4f
mungase-narayan <narayanmungase03@gmail.com>
2025-06-23 21:47:14 +0530

    add: routes

---
+++ src/routes.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+
+import RootLayout from "./layouts/root-layout";
+import AppLayout from "./layouts/app-layout";
+
+import HomePage from "./pages/home";
+
+const AppRoutes = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<RootLayout/>}>
+            <Route element={<AppLayout/>}>
+                <Route index element={<HomePage/>} />
+            </Route>
+        </Route>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AppRoutes
