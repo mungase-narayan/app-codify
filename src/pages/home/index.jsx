36fb449f9aed41e52aada28a94c4a008a1906db3
mungase-narayan <narayanmungase03@gmail.com>
2025-06-24 10:47:46 +0530

    add: sections

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,16 @@
 import SiteHeader from "@/components/shared/site-header";
 import HeroSection from "./components/hero-section";
+import FeaturesSection from "./components/features-section";
+import CtaSection from "./components/CTA-section";
+import Footer from "@/components/shared/footer";
 
 const HomePage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 overflow-x-hidden">
       <SiteHeader />
       <HeroSection />
+      <FeaturesSection />
+      <CtaSection />
     </div>
   );
 };
