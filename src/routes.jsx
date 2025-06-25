1faf27277fce052ed196be727c83dcc5763d485a
mungase-narayan <narayanmungase03@gmail.com>
2025-06-25 08:42:01 +0530

    add: auth routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,9 @@ import RootLayout from "./layouts/root-layout";
 import AppLayout from "./layouts/app-layout";
 
 import HomePage from "./pages/home";
+import AuthLayout from "./layouts/auth-layout";
+import SignInPage from "./pages/auth/sign-in";
+import SignUpPage from "./pages/auth/sign-up";
 
 const AppRoutes = () => {
   return (
@@ -12,6 +15,11 @@ const AppRoutes = () => {
         <Route path="/" element={<RootLayout />}>
           <Route element={<AppLayout />}>
             <Route index element={<HomePage />} />
+          </Route>
+
+          <Route path="auth" element={<AuthLayout />}>
+            <Route path="sign-in" element={<SignInPage />} />
+            <Route path="sign-up" element={<SignUpPage />} />
           </Route>
         </Route>
       </Routes>
