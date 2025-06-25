5df695c8d97898889bb30947a4d8d57ea24fdcb0
mungase-narayan <narayanmungase03@gmail.com>
2025-06-25 08:45:29 +0530

    fix: issue

--- src/layouts/auth-layout.jsx
+++ src/layouts/auth-layout.jsx
@@ -1,9 +1,8 @@
-import useAuth from "@/hooks/use-auth";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
-  const { isAuth } = useAuth();
-  if (isAuth) return <Navigate to="/" />;
+  // const { isAuth } = useAuth();
+  // if (isAuth) return <Navigate to="/" />;
   return <Outlet />;
 };
 
